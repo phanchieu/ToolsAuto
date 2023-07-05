cf4bc0e45732512e1a813ba40afa2786458e046b
phanchieu <phanchieudev@gmail.com>
2023-07-05 12:14:03 +0700

    'update1.1.0'

--- ToolOpenChrome/Program.cs
+++ ToolOpenChrome/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace ToolOpenChrome
+﻿using System.Diagnostics;
+using System.Security.Principal;
+
+namespace ToolOpenChrome
 {
     internal static class Program
     {
@@ -10,15 +13,29 @@
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
-            LoginForm loginForm = new LoginForm();
-
-            if (loginForm.ShowDialog() == DialogResult.OK)
+            if (IsRunAsAdmin())
             {
-                Application.Run(new FormMain());
+                ApplicationConfiguration.Initialize();
+                LoginForm loginForm = new LoginForm();
+
+                if (loginForm.ShowDialog() == DialogResult.OK)
+                {
+                    Application.Run(new FormMain());
+                }
             }
-            
+            else
+            {
+                MessageBox.Show("Ứng dụng cần chạy với quyền quản trị.", "Cảnh báo!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                Application.Exit();
+            }
+
             //Application.Run(new Form1());
+        }
+        private static bool IsRunAsAdmin()
+        {
+            WindowsIdentity identity = WindowsIdentity.GetCurrent();
+            WindowsPrincipal principal = new WindowsPrincipal(identity);
+            return principal.IsInRole(WindowsBuiltInRole.Administrator);
         }
     }
 }
